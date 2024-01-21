d7c014f11d6dcf2b37298c14d1b7411786bec3a2
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-01-21 23:43:55 +0900

    'None'

--- tasks.py
+++ tasks.py
@@ -36,3 +36,20 @@ def test_all(c):
         c.run(f"set PYTHONPATH=%PYTHONPATH%;{path} & python -m unittest discover")
     else:
         c.run(f'export PYTHONPATH="$PYTHONPATH:{path}" && python -m unittest')
+
+
+def translate(text):
+    import sys
+
+    path = os.path.abspath(os.path.dirname(__file__))
+    sys.path.append(path)
+    from src.translation.translation import translate_text
+
+    translated_text = translate_text(text, "ja", "en-GB")
+    print(translated_text)
+
+
+@task
+def commit(c, commit_type, message):
+    japanese_text = translate(message)
+    c.run(f"git commit -m '{commit_type}: {message}'")
